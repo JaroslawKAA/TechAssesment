af895ef4950c194b2568fbef667f94bc117c74a8
Jaroslaw Kaszczak <kaszczak.jaroslaw@outlook.com>
2021-09-08 14:02:42 +0200

    Init console app.

---
+++ AntsSimulator/AntsSimulator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AntsSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
